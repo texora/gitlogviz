ebf7f6a25733d60e486cdfd6af4495a9fb740102
texora <ultantaylor@gmail.com>
2023-12-11 03:33:39 +0900

    Update gitlogviz for flexible commit limit

--- gitlogviz/gitlogviz.py
+++ gitlogviz/gitlogviz.py
@@ -1,7 +1,10 @@
 import argparse
 import subprocess
+import tkinter as tk
+from tkinter import Scrollbar, Text
 
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
 def save_git_log_graph(commit_limit=10):
@@ -26,13 +29,54 @@ def save_git_log_graph(commit_limit=10):
     # Create a figure and axis for the plot
     fig, ax = plt.subplots(figsize=(10, 5))
 
+    # Determine the range of commits to display based on the commit_limit parameter
+    if isinstance(commit_limit, str) or commit_limit == 0:
+        # If commit_limit is 0 or not an integer, display all commits
+        commits = lines
+
+    elif commit_limit >= 1:
+        # If commit_limit is a positive integer, select the latest 'commit_limit' commits
+        commits = lines[:commit_limit]
+
+    elif commit_limit <= -1:
+        # If commit_limit is a negative integer, select the first 'abs(commit_limit)' commits
+        commits = lines[len(lines) + commit_limit :]
+
     # Display the first 'commit_limit' lines of the git log output as text on the plot
-    ax.text(0.1, 0.9, "\n".join(lines[:commit_limit]), va="top", fontfamily="monospace")
+    ax.text(0.1, 0.9, "\n".join(commits), va="top", fontfamily="monospace")
 
     # Set the aspect ratio, turn off axis, and display the plot
     ax.set_aspect("auto")
     ax.axis("off")
-    plt.show()
+
+    # Create a Tkinter window
+    root = tk.Tk()
+    root.title("Git Log Graph")
+
+    # Create a Canvas widget and pack it into the Tkinter window
+    canvas = FigureCanvasTkAgg(fig, master=root)
+    canvas_widget = canvas.get_tk_widget()
+    canvas_widget.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+    # Create a vertical scrollbar
+    v_scrollbar = Scrollbar(
+        root, orient=tk.VERTICAL, command=canvas.get_tk_widget().yview
+    )
+    v_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+    # Configure the canvas to use the scrollbar
+    canvas_widget.config(yscrollcommand=v_scrollbar.set)
+
+    # Create a Text widget for displaying commit information
+    text_widget = Text(root, height=10, width=50, wrap=tk.WORD)
+    text_widget.pack(side=tk.BOTTOM)
+
+    # Display the commit information in the Text widget
+    text_widget.insert(tk.END, "\n".join(commits))
+    text_widget.config(state=tk.DISABLED)  # Disable text editing
+
+    # Run the Tkinter main loop
+    root.mainloop()
 
 
 def main():
